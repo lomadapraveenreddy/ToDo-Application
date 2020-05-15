070f5d77fd8b4e76006689e625f08047cca0a8ef
lomadapraveenreddy <lomadapraveenreddy@gmail.com>
2020-05-15 19:30:36 +0530

    detail route added

--- todo/lib/routes/all_routes.dart
+++ todo/lib/routes/all_routes.dart
@@ -1,2 +1,3 @@
 export './intial_route.dart';
 export './new_to_do.dart';
+export './detail_route.dart';
--- todo/lib/widgets/to_do_card.dart
+++ todo/lib/widgets/to_do_card.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
+import 'package:todo/routes/all_routes.dart';
+import '../to_do_bloc/to_do_bloc.dart';
 import '../models/todo.dart';
 
 //This widget returns a card that takes ToDo object as argument
@@ -9,13 +12,18 @@ class ToDoCard extends StatelessWidget {
   ToDoCard(this.object);
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 15),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
-        child: ListTile(
-          title: Text(object.title),
-          subtitle: Text(DateFormat.yMMMd().format(object.deadline)),
+    return GestureDetector(
+      onTap: (){Navigator.of(context).pushNamed(DetailToDoView.routeName,arguments: object.id);
+      BlocProvider.of<ToDoBloc>(context).getByID(object.id);
+      },
+          child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 15),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+          child: ListTile(
+            title: Text(object.title),
+            subtitle: Text(DateFormat.yMMMd().format(object.deadline)),
+          ),
         ),
       ),
     );
